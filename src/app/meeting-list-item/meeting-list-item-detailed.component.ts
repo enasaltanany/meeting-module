c0809a4bcf800450ffbcd9194935c5a9bb89fdac
enasaltanany <enasaltanany1172@outlook.com>
2017-10-09 10:55:41 +0200

    collapsedRows

---
+++ src/app/meeting-list-item/meeting-list-item-detailed.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+
+import { MeetingModel } from '../meeting-info/meeting-info.component';
+
+@Component({
+  selector: '[app-meeting-list-item-detailed]',
+  templateUrl: './meeting-list-item-detailed.component.html',
+  styleUrls: ['./meeting-list-item-detailed.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class MeetingListItemDetailedComponent implements OnInit {
+
+  @Input()
+  meeting;
+
+  public expanded : boolean = true;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/meeting-list-item/meeting-list-item.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
+
+import { MeetingModel } from '../meeting-info/meeting-info.component';
+
+@Component({
+  selector: '[app-meeting-list-item]',
+  templateUrl: './meeting-list-item.component.html',
+  styleUrls: ['./meeting-list-item.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class MeetingListItemComponent implements OnInit {
+
+  @Input()
+  meeting;
+
+  @Output()
+  expand = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  expandClicked() {
+    this.expand.next(this.meeting);
+  }
+}
